bfb1841a346110aa2f14431c6904ad78406ef065
medigakoushik <medigasaikoushik@gmail.com>
2019-06-18 14:52:48 +0530

    added toggling to the + button

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -44,3 +44,9 @@ $("input[type='text']").keypress(function(event){
      $("ul").append("<li><span><i class='fa fa-trash'></i></span> "  + todoText + "</li>");
 	}
 });
+
+//toggling +
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+
+})
