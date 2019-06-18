bf95f309e0c2b770441c8862ec1d656dea8cc23e
medigakoushik <medigasaikoushik@gmail.com>
2019-06-18 13:58:56 +0530

    adding todos first step , declared a variable to store the new todo , it is stored only when the user presses enter key

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -31,3 +31,12 @@ $("span").click(function(event){
 	});
 	event.stopPropagation();
 });
+
+
+//creating new Todos
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+     var todoText = $(this).val();
+	}
+});
